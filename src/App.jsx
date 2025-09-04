1b2647c122331034699089d090126a41bf43ef08
adilhan <adilhansatymkulov40@gmaill.com>
2025-09-04 22:48:30 +0600

    the last commit today.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import './App.css';
 import Footer from "./components/Footer";
 import Hero from "./components/Home/hero";
 
-import LifeOverview from "./components/Life/LifeOverview";
-import ClubsSection from "./components/Life/ClubsSection";
-import InternationalStudents from "./components/Life/InternationalStudents";
+import About from "./components/About/About";
+import Management from "./components/About/Management";
+import Documents from "./components/About/Documents";
+import Careers from "./components/About/Careers";
+import Partners from "./components/About/Partners";
 
 import Academics from './components/academics/academics'
 import Faculties from './components/academics/faculties'
 import Deparments from './components/academics/departments'
 import Calendar from './components/academics/calendar'
 import Resources from "./components/academics/resources";
+
+import LifeOverview from "./components/Life/LifeOverview";
+import ClubsSection from "./components/Life/ClubsSection";
+import InternationalStudents from "./components/Life/InternationalStudents";
 import Gallery from "./components/Life/Gallery";
 
 // Страницы-заглушки
@@ -28,11 +34,11 @@ function App() {
           <Routes>
             {/* About */}
             <Route path="/" element={<Hero />} />
-            <Route path="/about" element={<Page title="About University" />} />
-            <Route path="/about/management" element={<Page title="Management" />} />
-            <Route path="/about/documents" element={<Page title="Documents" />} />
-            <Route path="/about/vacancies" element={<Page title="Vacancies" />} />
-            <Route path="/about/partners" element={<Page title="Partners" />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/about/management" element={<Management />} />
+            <Route path="/about/documents" element={<Documents />} />
+            <Route path="/about/vacancies" element={<Careers />} />
+            <Route path="/about/partners" element={<Partners />} />
 
             {/* Academics */}
             <Route path="/academics" element={<Academics />} />
@@ -41,19 +47,19 @@ function App() {
             <Route path="/academics/calendar" element={<Calendar />} />
             <Route path="/academics/resources" element={<Resources />} />
 
-          {/* Admission */}
-          <Route path="/admission" element={<Page title="Admission Process" />} />
-          <Route path="/admission/requirements" element={<Page title="Requirements" />} />
-          <Route path="/admission/tuition" element={<Page title="Tuition" />} />
-          <Route path="/admission/scholarships" element={<Page title="Scholarships" />} />
-          <Route path="/admission/apply" element={<Page title="Apply Online" />} />
+            {/* Admission */}
+            <Route path="/admission" element={<Page title="Admission Process" />} />
+            <Route path="/admission/requirements" element={<Page title="Requirements" />} />
+            <Route path="/admission/tuition" element={<Page title="Tuition" />} />
+            <Route path="/admission/scholarships" element={<Page title="Scholarships" />} />
+            <Route path="/admission/apply" element={<Page title="Apply Online" />} />
 
-          {/* Research */}
-          <Route path="/research" element={<Page title="Research Areas" />} />
-          <Route path="/research/centers" element={<Page title="Research Centers" />} />
-          <Route path="/research/publications" element={<Page title="Publications" />} />
-          <Route path="/research/conferences" element={<Page title="Conferences" />} />
-          <Route path="/research/grants" element={<Page title="Grants" />} />
+            {/* Research */}
+            <Route path="/research" element={<Page title="Research Areas" />} />
+            <Route path="/research/centers" element={<Page title="Research Centers" />} />
+            <Route path="/research/publications" element={<Page title="Publications" />} />
+            <Route path="/research/conferences" element={<Page title="Conferences" />} />
+            <Route path="/research/grants" element={<Page title="Grants" />} />
 
             {/* Campus Life */}
             <Route path="/campus-life" element={<LifeOverview />} />
@@ -62,18 +68,19 @@ function App() {
             <Route path="/campus-life/gallery" element={<Gallery/>} />
             <Route path="/campus-life/international" element={<InternationalStudents/>} />
 
-          {/* News */}
-          <Route path="/news" element={<Page title="All News" />} />
-          <Route path="/news/events" element={<Page title="News Events" />} />
-          <Route path="/news/announcements" element={<Page title="Announcements" />} />
-          <Route path="/news/articles" element={<Page title="Articles" />} />
+            {/* News */}
+            <Route path="/news" element={<Page title="All News" />} />
+            <Route path="/news/events" element={<Page title="News Events" />} />
+            <Route path="/news/announcements" element={<Page title="Announcements" />} />
+            <Route path="/news/articles" element={<Page title="Articles" />} />
 
-          {/* Contacts */}
-          <Route path="/contacts" element={<Page title="Contacts" />} />
-          <Route path="/contacts/admission" element={<Page title="Admission Office" />} />
-          <Route path="/contacts/map" element={<Page title="Map" />} />
-          <Route path="/contacts/feedback" element={<Page title="Feedback" />} />
-        </Routes>
+            {/* Contacts */}
+            <Route path="/contacts" element={<Page title="Contacts" />} />
+            <Route path="/contacts/admission" element={<Page title="Admission Office" />} />
+            <Route path="/contacts/map" element={<Page title="Map" />} />
+            <Route path="/contacts/feedback" element={<Page title="Feedback" />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
