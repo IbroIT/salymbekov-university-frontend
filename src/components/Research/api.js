87da914d3ce730bd27db96a36f7516cd3e683a17
iliyar <iliyarmidinov11@gmail.com>
2025-09-06 12:06:45 +0600

    public

---
+++ src/components/Research/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const publicationsApi = {
+  // Получение публикаций с фильтрами
+  getPublications: (params) => api.get('/publications/', { params }),
+  
+  // Получение одной публикации
+  getPublication: (id) => api.get(`/publications/${id}/`),
+  
+  // Создание публикации
+  createPublication: (data) => api.post('/publications/', data),
+  
+  // Обновление публикации
+  updatePublication: (id, data) => api.put(`/publications/${id}/`, data),
+  
+  // Удаление публикации
+  deletePublication: (id) => api.delete(`/publications/${id}/`),
+};
+
+export const researchCentersApi = {
+  // Получение центров
+  getResearchCenters: () => api.get('/research-centers/'),
+};
+
+export default api;
